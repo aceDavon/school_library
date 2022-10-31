ca0a23057aefd8922d726c3a61b333f3d575f11e
aceDavon <makojidavid@gmail.com>
2022-10-31 16:19:26 +0100

    Fix Linter errors on all classes
    Rename files to use snake casing and also refactor code to remove useless
     variable assignments

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
-require "./Person.rb"
+require './Person'
 
 class Student < Person
   def initialize(classroom)
-    super(name, age, parent_permission = true)
+    super(name, age, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯(ツ)/¯'
   end
 end
